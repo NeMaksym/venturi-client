a417d45df174d239abbd4e50b6de374e2d8a920b
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2025-09-19 14:50:06 +0300

    Add DB reset to debug page

--- src/pages/DebugPage.tsx
+++ src/pages/DebugPage.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import Button from '@mui/material/Button'
 
 import { PageLayout } from '../components'
+import { DBProvider, DB_CONFIG } from '../db/provider'
 
 export const DebugPage: React.FC = () => {
-    return <PageLayout title="Debug Information"></PageLayout>
+    const handleResetDatabase = () => {
+        if ('indexedDB' in window) {
+            DBProvider.instance.close()
+
+            const deleteResult = indexedDB.deleteDatabase(DB_CONFIG.name)
+
+            deleteResult.onsuccess = () => {
+                window.location.reload()
+            }
+
+            deleteResult.onerror = () => {
+                console.error('Failed to delete database')
+            }
+        }
+    }
+
+    return (
+        <PageLayout title="Debug Information">
+            <Button
+                sx={{ width: '200px' }}
+                variant="contained"
+                color="error"
+                onClick={handleResetDatabase}
+            >
+                Reset Database
+            </Button>
+        </PageLayout>
+    )
 }
